cce1c620aa61f18bf1ba00bb85ff148ebbb84991
toho36 <toho0036@gmail.com>
2024-12-12 09:19:35 +0100

    button styling

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,16 +1,18 @@
-import Link, { LinkProps } from 'next/link';
+import Link, { LinkProps } from "next/link";
 
 export interface ButtonProps extends LinkProps {
 	children: React.ReactNode;
-	color: 'primary' | 'secondary';
+	color: "primary" | "secondary";
 }
 
 export default function Button({ children, color, ...props }: ButtonProps) {
 	return (
 		<Link
 			{...props}
-			className={`p-2.5 rounded-lg font-bold ${
-				color === 'primary' ? 'bg-brand-primary text-text-tertiary' : 'bg-pink-200 text-amber-700'
+			className={`p-2.5 rounded-lg font-bold border-2 transition-colors ${
+				color === "primary"
+					? "bg-brand-primary text-text-tertiary border-transparent hover:bg-brand-primary-hover active:bg-brand-primary-active"
+					: "bg-white text-brand-primary border-brand-primary hover:text-brand-primary-hover hover:border-brand-primary-hover active:text-brand-primary-active active:border-brand-primary-active"
 			}`}
 		>
 			{children}
