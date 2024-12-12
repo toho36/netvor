a7b0ddada5dd1a1390aee25039c51d57ebb37c92
toho36 <toho0036@gmail.com>
2024-12-12 09:42:08 +0100

    card styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,54 @@
 import Card from "@/components/Card";
-import Button from "@/components/Button";
 
 export default function Home() {
 	return (
-		<div className="container mx-auto my-20">
+		<div className="container mx-auto ">
 			<div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-				{/* Place the Card here. */}
 				<div>
 					<Card
 						imageSrc="https://images.squarespace-cdn.com/content/v1/607f89e638219e13eee71b1e/1684821560422-SD5V37BAG28BURTLIXUQ/michael-sum-LEpfefQf4rU-unsplash.jpg"
 						title="gatto"
 						titleLink="https://www.hphhfoundation.org/blog/origin-of-cats"
-						description="This is an optional description for the card."
+						primaryAction={{ label: "Save", link: "/save", color: "primary" }}
+						secondaryAction={{
+							label: "Share",
+							link: "/share",
+							color: "secondary",
+						}}
+					/>
+				</div>
+				<div>
+					<Card
+						imageSrc="https://images.squarespace-cdn.com/content/v1/607f89e638219e13eee71b1e/1684821560422-SD5V37BAG28BURTLIXUQ/michael-sum-LEpfefQf4rU-unsplash.jpg"
+						title="gatto"
+						titleLink="https://www.hphhfoundation.org/blog/origin-of-cats"
+						primaryAction={{ label: "Save", link: "/save", color: "primary" }}
+						secondaryAction={{
+							label: "Share",
+							link: "/share",
+							color: "secondary",
+						}}
+						description=" short description
+						"
+					/>
+				</div>
+
+				<div>
+					<Card
+						imageSrc="https://images.squarespace-cdn.com/content/v1/607f89e638219e13eee71b1e/1684821560422-SD5V37BAG28BURTLIXUQ/michael-sum-LEpfefQf4rU-unsplash.jpg"
+						title="gatto"
+						titleLink="https://www.hphhfoundation.org/blog/origin-of-cats"
+						primaryAction={{ label: "Save", link: "/save", color: "primary" }}
+						secondaryAction={{
+							label: "Share",
+							link: "/share",
+							color: "secondary",
+						}}
+						description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra, lacus nec luctus vehicula, erat ipsum feugiat est, sed tincidunt arcu nulla et erat. Nulla facilisi. Integer non lectus non lorem efficitur vulputate. Vestibulum feugiat dictum quam, vel dictum arcu fringilla nec. Cras rhoncus ante vel nisl aliquet, nec aliquet lacus vulputate. Ut vestibulum luctus leo at tristique. Nam pharetra, nunc in fermentum bibendum, turpis dolor auctor lorem, nec tincidunt velit orci at sapien.
+						"
 					/>
 				</div>
 			</div>
-			<Button color="primary" href="#">
-				Primary
-			</Button>
-			<Button color="secondary" href="#">
-				Primary
-			</Button>
 		</div>
 	);
 }
--- components/Card.tsx
+++ components/Card.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link";
 import Image from "next/image";
-import Button, { ButtonProps } from "./Button";
+import Button, { ButtonProps } from "@/components/Button";
 
-type Action = {
+interface Action {
 	label: string;
 	link: string;
 	color: ButtonProps["color"];
-};
+}
 
 interface CardProps {
 	imageSrc?: string;
@@ -26,42 +26,40 @@ export default function Card({
 	secondaryAction,
 }: CardProps) {
 	return (
-		<div
-			className="group relative flex flex-col overflow-hidden rounded-tl-lg rounded-tr-lg rounded-bl-none rounded-br-none border-t-0 border-l-0 border-b-0 border-r-0 border-gray-200 bg-white shadow-md hover:shadow-lg focus-within:ring-2 focus-within:ring-blue-500 focus-within:ring-offset-2 w-[332px] h-[360px] p-4 gap-5"
-			tabIndex={0}
-			role="button"
-		>
-			{imageSrc && (
-				<Image
-					src={imageSrc}
-					alt="Illustrative image"
-					width={400}
-					height={200}
-					className="h-48 w-full object-cover"
-				/>
-			)}
-			<div className="p-4">
-				<h3 className="text-lg font-bold group-hover:underline">
-					<Link href={titleLink} passHref>
+		<Link href={titleLink}>
+			<article className="group relative flex flex-col overflow-hidden rounded-tl-lg rounded-tr-lg rounded-bl-none rounded-br-none border-t-0 border-l-0 border-b-0 border-r-0 border-gray-200 bg-white shadow-md hover:shadow-lg focus-within:ring-2 focus-within:ring-blue-500 focus-within:ring-offset-2 w-[332px]  p-4 gap-5">
+				{imageSrc && (
+					<div className="w-full h-48 overflow-hidden">
+						<Image
+							src={imageSrc}
+							alt=""
+							width={400}
+							height={200}
+							className="h-full w-full object-cover"
+						/>
+					</div>
+				)}
+				<div className="p-4">
+					<h3 className="text-lg font-bold group-hover:underline focus:outline-none">
 						{title}
-					</Link>
-				</h3>
-				{description && (
-					<p className="mt-2 text-sm text-gray-600">{description}</p>
-				)}
-			</div>
-			<div className="mt-auto flex justify-end gap-2 p-4">
-				{primaryAction && (
-					<Button href={primaryAction.link} color={primaryAction.color}>
-						{primaryAction.label}
-					</Button>
-				)}
-				{secondaryAction && (
-					<Button href={secondaryAction.link} color={secondaryAction.color}>
-						{secondaryAction.label}
-					</Button>
-				)}
-			</div>
-		</div>
+					</h3>
+					{description && (
+						<p className="mt-2 text-sm text-gray-600">{description}</p>
+					)}
+				</div>
+				<footer className="mt-auto flex justify-end gap-2 p-4">
+					{secondaryAction && (
+						<Button color={secondaryAction.color} href={secondaryAction.link}>
+							{secondaryAction.label}
+						</Button>
+					)}
+					{primaryAction && (
+						<Button color={primaryAction.color} href={primaryAction.link}>
+							{primaryAction.label}
+						</Button>
+					)}
+				</footer>
+			</article>
+		</Link>
 	);
 }
